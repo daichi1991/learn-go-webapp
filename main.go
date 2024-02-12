1e44d20069782ef40d352a437f0d4bbbea2757bf
daichi1991 <ks-pan@live.jp>
2024-02-12 12:18:38 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"fmt"
+	"github.com/daichi1991/learn-go-webapp/handlers"
+)
+
+func main() {
+	http.HandleFunc("/hello", handlers.helloHandler)
+	http.HandleFunc("/article", handlers.postArticleHandler)
+	http.HandleFunc("/article/list", handlers.articleListHandler)
+	http.HandleFunc("/article/1", handlers.articleDetailHandler)
+	http.HandleFunc("/article/nice", handlers.postNiceHandler)
+	http.HandleFunc("/comment", handlers.postCommentHandler)
+
+	log.Println("server start at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
